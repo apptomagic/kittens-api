6986e3657f01357ecc098723670d245aa0392edc
Lalo Martins <lalo.martins@gmail.com>
2019-05-28 20:53:04 +0300

    add simple paging (windowing) to posts fetch

--- examples/curl/posts.sh
+++ examples/curl/posts.sh
@@ -10,8 +10,21 @@ echo "Create a reply"
 REPLY=$(grpcurl -plaintext -d '{"text": "bar", "inReplyTo": "'"${OP}"'", "authorDisplayName": "Bob"}' localhost:50051 kittens.post.Posts/Create | jq -r .id)
 echo "Created reply: ${REPLY}"
 
+echo "Create a few more posts in the conversation"
+REPLY2=$(grpcurl -plaintext -d '{"text": "bar", "inReplyTo": "'"${OP}"'", "authorDisplayName": "Jane"}' localhost:50051 kittens.post.Posts/Create | jq -r .id)
+grpcurl -plaintext -d '{"text": "bar", "inReplyTo": "'"${REPLY}"'", "authorDisplayName": "Anonymous Coward"}' localhost:50051 kittens.post.Posts/Create
+ANNE=$(grpcurl -plaintext -d '{"text": "bar", "inReplyTo": "'"${OP}"'", "authorDisplayName": "Anne Onymous"}' localhost:50051 kittens.post.Posts/Create | jq -r .created)
+grpcurl -plaintext -d '{"text": "bar", "inReplyTo": "'"${REPLY2}"'", "authorDisplayName": "Anonymous Coward"}' localhost:50051 kittens.post.Posts/Create
+grpcurl -plaintext -d '{"text": "bar", "inReplyTo": "'"${REPLY2}"'", "authorDisplayName": "Bob"}' localhost:50051 kittens.post.Posts/Create
+
 echo "Get all posts by Anonymous Coward"
 grpcurl -plaintext -d '{"authorId": "Anonymous Coward"}' localhost:50051 kittens.post.Posts/PostsByUser
 
-echo "Get all posts in our new conversation"
+echo "Get all posts in our conversation"
 grpcurl -plaintext -d '{"postId": "'"${OP}"'"}' localhost:50051 kittens.post.Posts/Thread
+
+echo "Get all posts in our conversation after Jane's"
+grpcurl -plaintext -d '{"postId": "'"${OP}"'", "after": "'"${REPLY2}"'"}' localhost:50051 kittens.post.Posts/Thread
+
+echo "Get all posts in our conversation since Anne's (${ANNE})"
+grpcurl -plaintext -d '{"postId": "'"${OP}"'", "since": "'"${ANNE}"'"}' localhost:50051 kittens.post.Posts/Thread
